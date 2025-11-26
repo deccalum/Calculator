b26c42021d179a9ee058edcb5ace567b9bdd82ac
Dennis Calmius <dcalmius@gmail.com>
2025-11-26 09:16:01 +0100

    only notes

---
+++ src/main/java/se/lexicon/Main.java
@@ -0,0 +1,17 @@
+package se.lexicon;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    static void main() {
+        //Basic calculation
+        // add. sub. mult. div.
+        // requires input
+        // able to perform multiple operations and exit
+
+        // handle/print errors
+        // advanced; square, eqvations, constants etc.
+        // Simple GUI. (Swing, JAVAFX)
+        }
+    }
+}
