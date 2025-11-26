f7f9ea3948f1083c39fb43cc3ce352ae8509381e
Dennis Calmius <dcalmius@gmail.com>
2025-11-26 14:26:22 +0100

    added complex calculator using exp4j library

--- src/main/java/se/lexicon/Main.java
+++ src/main/java/se/lexicon/Main.java
@@ -1,22 +1,52 @@
 package se.lexicon;
 
 import java.util.Scanner;
+import net.objecthunter.exp4j.Expression;
+import net.objecthunter.exp4j.ExpressionBuilder;
+
 public class Main {
+
+    public class FunctionEvaluator {
+
+        public static double evaluateMathString(String expressionString) {
+            Expression expression = new ExpressionBuilder(expressionString).build();
+            return expression.evaluate();
+        }
+    }
     public static void main(String[] args) {
-        
-        System.out.printf("Calculator%n");
         boolean run = true;
         Scanner scanner = new Scanner(System.in);
+        
         while (run) {
+            System.out.printf("Simple Calculator [1] | Complex Calculator [2] | Exit [X]%n"); 
+            
+            String choice = scanner.next();
+
+            if (choice.equals("1")) {
+                simpleCalculator(scanner);
+            } else if (choice.equals("2")) {
+                complexCalculator(scanner);
+            } else if (choice.equalsIgnoreCase("X")) {
+                run = false;
+            }
             System.out.println();
+        }
+        scanner.close();
+        System.out.printf("Closed%n");
+    }
+
+    private static void simpleCalculator(Scanner scanner) {
+        boolean continueCalculation = true;
+
+        while (continueCalculation) {
+        System.out.printf("Simple Calculator Selected.%n");
+
                 System.out.printf("Enter your first number: ");
-                if (scanner.hasNext("X")) {
-                    run = false;
-                    continue;
-                }
                 double num1 = scanner.nextDouble();
+
                 System.out.print("Enter operator (+, -, *, /): ");
                 String operator = scanner.next();
+
                 System.out.printf("Enter your second number: ");
                 double num2 = scanner.nextDouble();
 
@@ -37,23 +67,31 @@ public class Main {
                     System.out.println();
                     System.out.printf("Result: %.2f%n", num1 / num2);
                     break;
-                default:
-                    System.out.println("Invalid operator.");
-                    break;
             }
             scanner.nextLine();
-            System.out.printf("%n Run again [ENTER] | exit [X] : ");
-            System.out.println();
-            System.out.println();
-            System.out.println();
-            System.out.println();
-            System.out.println();
-            String exitInput = scanner.nextLine();
-            if (exitInput.equalsIgnoreCase("exit")) {
-                run = false;
+            System.out.print("Any key [REPEAT] | Exit [X]: ");
+            String continueChoice = scanner.nextLine();
+            if (continueChoice.equalsIgnoreCase("X")) {
+                continueCalculation = false;
             }
         }
-        scanner.close();
-        System.out.printf("Calculator closed.%n");
+    }
+    
+    private static void complexCalculator(Scanner scanner) {
+        boolean continueCalculation = true;
+        while (continueCalculation) {
+        System.out.printf("Complex Calculator Selected.%n");
+        System.out.printf("Enter any legit expression, accepts +, -, *, /, ^ and parentheses:%n");
+        scanner.nextLine(); 
+        String expression = scanner.nextLine();
+
+        double result = FunctionEvaluator.evaluateMathString(expression);;
+        System.out.printf("Result: %.2f%n", result);
+            System.out.print("Any key [REPEAT] | Exit [X]: ");
+            String continueChoice = scanner.nextLine();
+            if (continueChoice.equalsIgnoreCase("X")) {
+                continueCalculation = false;
+            }
+        }
     }
 }
