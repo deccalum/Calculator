35b5fdae051fba58f93c27847fb1a9bf98cb05cd
Dennis Calmius <dcalmius@gmail.com>
2025-11-26 10:11:45 +0100

    exit function

--- src/main/java/se/lexicon/Main.java
+++ src/main/java/se/lexicon/Main.java
@@ -1,17 +1,33 @@
 package se.lexicon;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+import java.util.Scanner;
+
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    static void main() {
+    public static void main(String[] args) {
+        boolean run = true;
+        Scanner scanner = new Scanner(System.in);
+        while (run) {
+            System.out.printf("Calculator%n");
+            System.out.printf("Enter calculus: ");
+
+            scanner.nextLine();
+
+            int choice = scanner.nextInt();
+
+            if (choice == 0) run = false;
+        }
+        System.out.printf("Exiting...");
         //Basic calculation
         // add. sub. mult. div.
+
+        // need to read numbers with nextDouble()?
         // requires input
+        // read only nuumbers or operators
         // able to perform multiple operations and exit
 
         // handle/print errors
-        // advanced; square, eqvations, constants etc.
+        // advanced; square, eqvations, constants, physics etc.
         // Simple GUI. (Swing, JAVAFX)
         }
     }
-}
